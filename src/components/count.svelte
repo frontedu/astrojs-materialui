d1bfc3eb381e6dfa3445233fc171be4c72e42c70
Eduardo F. Silva <frontedu@icloud.com>
2023-05-22 15:49:19 -0300

    svelte impl

---
+++ src/components/count.svelte
@@ -0,0 +1,16 @@
+<script>
+	let count = 0;
+
+	$: if (count >= 10) {
+		alert(`count is dangerously high!`);
+		count = 9;
+	}
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button class="mt-4 bg-black text-white p-2 rounded-md w-[100%]" on:click={handleClick}>
+	Clicado {count} {count === 1 ? 'vez' : 'vezes'}
+</button>
