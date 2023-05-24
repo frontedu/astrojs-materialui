87db4f8217574a3aab80ed9902253ce1477dc365
Eduardo F. Silva <frontedu@icloud.com>
2023-05-24 15:58:59 -0300

    novo login em react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), preact(), react(), image()]
 });
