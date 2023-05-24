00d91a8c6423ebfdaae2337a838d612d5d259d11
Eduardo F. Silva <frontedu@icloud.com>
2023-05-24 16:09:35 -0300

    rm svelte e preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), preact(), react(), image()]
+  integrations: [tailwind(), react(), image()]
 });
